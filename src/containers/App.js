56e2d091a0394e3be845b260c6cfcbb2496c662c
Anton Ayvarov <ayvaroff@gmail.com>
2019-05-02 13:22:10 +0300

    * initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  classes: PropTypes.object
+}
+
+export default App
